0b1b28784c64398bf047c5842b6d0672fe6de31f
Matt Johnson <everettmattjohnson@gmail.com>
2020-09-30 00:11:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,22 @@ function deleteItems() {
         $(event.target).closest('li').remove();
     });
 }
-
 function addItems() {
     $('#js-shopping-list-form').on('submit', event => {
         event.preventDefault();
         event.stopPropagation();
-        let inputVal = $(".js-shopping-list-entry").val();
-        $('ul.shopping-list').append("<li>
-        <span class="shopping-item">${inputVal}</li>")
+        let userText = $(".js-shopping-list-entry").val();
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item">Sample</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+        </li>`);
     });
 }
 
